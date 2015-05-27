9bfd625532f8cff75372ea687fb5bf7130e2c0de
Lee Gatchell <leegatchell@gmail.com>
2015-05-27 17:39:22 -0600

    Added Nickel Value Increase by Five

--- VendingMachine.java
+++ VendingMachine.java
@@ -5,11 +5,33 @@ public class VendingMachine {
 	/**
 	 * Stores a vending machine's inventory
 	 */
-
+	private Integer numNickels = 0;
+	private Integer numDimes = 0;
+	private Integer numQuarters = 0;
+	
+	private String getValue(){
+		if(numNickels==0&&numDimes==0&&numQuarters==0){
+			return "INSERT COIN";
+		}
+		else{
+			return String.valueOf((numNickels*5)+(numDimes)*10+(numQuarters*25));
+		}
+	}
+	
 	public String insert(Integer weight, Integer diameter) {
 		/**
 		 * @return INSERT COIN and additional text to pass the test
 		 */
-		return "INSERT COIN AND HEY THIS IS DIFFERENT";
+		if(weight==1&diameter==1){
+			numNickels++;
+		}
+		return getValue();
 	}
+	public String insert() {
+		/**
+		 * @return total value
+		 */
+		return getValue();
+	}
+	
 }
--- VendingMachineTest.java
+++ VendingMachineTest.java
@@ -1,6 +1,7 @@
 package com.pillar.eval;
 import org.junit.Test;
 import static junit.framework.TestCase.assertFalse;
+import static junit.framework.TestCase.assertEquals;
 
 public class VendingMachineTest {
 
@@ -8,5 +9,12 @@ public class VendingMachineTest {
 	public void whenCoinsAreInsertedInsertCoinIsNotDisplayed() {
 		VendingMachine vm1 = new VendingMachine();
 		assertFalse(vm1.insert(1,1).equals("INSERT COIN"));
+	}
+	
+	@Test
+	public void whenNickelIsInsertedValueIncreasesByFive() {
+		VendingMachine vm2 = new VendingMachine();
+		assertEquals("5", vm2.insert(1,1));
+		assertEquals("10", vm2.insert(1,1));
 	}
 }
